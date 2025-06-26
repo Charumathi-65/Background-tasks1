5b0db19b03f543a36b4145b5c52fe9439904d301
Charumathi-65 <charug7373@gmail.com>
2025-06-26 20:43:53 +0530

    changes in celery

--- myproject/celery.py
+++ myproject/celery.py
@@ -6,4 +6,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 app = Celery("myproject")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
-app.conf.broker_url = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+app.conf.broker_url = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
+app.conf.result_backend = app.conf.broker_url
